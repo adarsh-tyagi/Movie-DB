0bcfb95243ce78615fdb712c84cd4cf4f7ed9c90
Adarsh Tyagi <adarshtyagi044@gmail.com>
2022-01-29 23:18:32 +0530

    started: frontend

--- frontend/src/context.js
+++ frontend/src/context.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useReducer, useContext } from "react";
 import reducer from "./reducer";
-
 const AppContext = React.createContext();
+
+const url =
+  "https://api.themoviedb.org/3/movie/popular?api_key=e8dce7da46844c6b013c0e835f59da30";
 
 const initialState = {
   loading: false,
@@ -15,15 +17,12 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {}, []);
-  return (
-    <AppContext.Provider value={{ }}>
-      {children}
-    </AppContext.Provider>
-  );
+
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
 
 export { AppContext, AppProvider };
